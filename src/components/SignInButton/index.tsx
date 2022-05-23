78285ca78924b28c1fff822ac8d9cc1068121788
Matheus Filype <matheus.campos@gcbinvestimentos.com>
2022-05-23 11:58:35 -0300

    feat: Authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,18 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Santosl2 <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name} <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={() => signIn("github")}>
       <FaGithub color="#eba417" />
       Sign in with Github{" "}
     </button>
