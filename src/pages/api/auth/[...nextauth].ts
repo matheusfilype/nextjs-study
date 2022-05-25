733e2f47af18153b689059216bb89d625b17a458
Matheus Filype <matheus.campos@gcbinvestimentos.com>
2022-05-25 11:13:47 -0300

    feat: Fauna Integretion

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query } from "faunadb";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,4 +11,31 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(query.Index("user_by_email"), query.Casefold(email))
+              )
+            ),
+            query.Create(query.Collection("users"), {
+              data: { email },
+            }),
+            query.Get(
+              query.Match(query.Index("user_by_email"), query.Casefold(email))
+            )
+          )
+        );
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+  },
 });
